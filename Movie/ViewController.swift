a93c083b8aef606f60247d4cf313d9f5e7a9f512
Alex <sanya1444@gmail.com>
2020-10-23 11:49:15 +0300

    Initial Commit

---
+++ Movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie
+//
+//  Created by Oleksandr Karpenko on 23.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
